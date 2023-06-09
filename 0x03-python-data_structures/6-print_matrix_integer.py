22e97408cbdd50cf979e4c88cc6dbdd0111d5bf0
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-09 03:01:55 -0700

    Matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    full_len = len(matrix)
+    inner_len = len(matrix[0])
+    for i in range(full_len):
+        for j in range(inner_len):
+            if j == inner_len - 1:
+                print("{}".format(matrix[i][j]))
+                continue
+            print("{}".format(matrix[i][j]), end=" ")
