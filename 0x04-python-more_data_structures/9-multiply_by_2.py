82d6c3c79b25e1943950428a07033cf9d4a6fe0f
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-14 07:20:12 -0700

    Stop mutating original dictionary in multiply by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for j in list(a_dictionary):
-        a_dictionary[j] = 2 * a_dictionary[j]
-    return a_dictionary
+        new_dict[j] = 2 * a_dictionary[j]
+    return new_dict
