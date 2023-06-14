f404e2f2629f64f5a28553f48a15b86fbe1dc65b
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-14 07:28:05 -0700

    Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    starter = 0
+    for i in list(a_dictionary):
+        if a_dictionary[i] >= starter:
+            starter = a_dictionary[i]
+            max_key = i
+    return max_key
