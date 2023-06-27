80f27c9dace315b79fc24efe5e4476f2e9a6cc69
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-27 12:09:06 -0700

    Create square, allow size speficiation and confirm type

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 2-square.py
+"""Square class with initialization"""
+
+
+class Square:
+    """Square template"""
+
+    def __init__(self, size=0):
+        """Define initialization function
+        Args:
+            size(int): New square size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
