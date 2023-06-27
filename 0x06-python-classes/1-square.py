7da4ff3062a1485c4a8c017581e868f7ea22088d
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-27 11:24:02 -0700

    Document the init function

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,5 +2,9 @@
 class Square:
     """A template for square objets"""
     def __init__(self, self_size):
-        """Initialize a square"""
+        """
+        Initialize a square instance
+        Args:
+            size (int): New square size
+        """
         self.__size = self_size
