c0b3bf0c5306eb4ee855e43f7e6a0dc6aa1b4f5f
Ay-slim <ayooluwaadedipe@gmail.com>
2023-06-27 15:34:26 -0700

    Fix __size

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
             size(int): New square size
         """
-        self.size = size
+        self.__size = size
     
     def size(self):
         """Return size private attribrute"""
