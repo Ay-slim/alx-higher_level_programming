fa4cf38ea1feb0311f9e614d90a3b52ce83296e2
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-05 14:14:15 -0700

    Correct max integer tests

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,38 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
+"""Unittest for max_integer([..])"""
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    def regular_list_test(self):
-        """Test for max integer function"""
-        self.assertEqual(max_integer([2, 4, 6, 8], 8))
 
-    def scrambled_list_test(self):
+class TestMaxInteger(unittest.TestCase):
+    def test_max_at_the_end(self):
         """Test for max integer function"""
-        self.assertEqual(max_integer([4, 2, 8, 6], 8))
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_max_at_the_beginning(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+
+    def test_max_in_the_middle(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([1, 4, 3]), 4)
+
+    def test_one_negative_number(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([1, -2, 3, 4]), 4)
+
+    def tests_only_negative_numbers(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_list_of_one_element(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([4]), 4)
+
+    def test_list_is_empty(self):
+        """Test for max integer function"""
+        self.assertEqual(max_integer([]), None)
+
+if __name__ == '__main__':
+    unittest.main()
