68ff13fa2cae7e0471beb6a028d992a37f83fe4a
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-10 10:36:47 -0700

    Implement def area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for geometry class creation"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
