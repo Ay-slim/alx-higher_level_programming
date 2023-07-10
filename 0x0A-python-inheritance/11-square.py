817199d21ff689f187de06489efa8023c7eedc99
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-10 14:43:04 -0700

    New print method

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class setup"""
+
+    def __init__(self, size):
+        """Initialization of Square class"""
+
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Function to calculate square area"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """Print square description"""
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
