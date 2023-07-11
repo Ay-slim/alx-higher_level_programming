1c633ad343fa5865bae80054c748c66f153506af
Ay-slim <ayooluwaadedipe@gmail.com>
2023-07-11 10:39:04 -0700

    Add item to a list and write to json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module to add all arguments to a python list"""
+
+
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+for i in range(1, len(sys.argv)):
+    my_list.append(str(sys.argv[i]))
+save_to_json_file(my_list, filename)
