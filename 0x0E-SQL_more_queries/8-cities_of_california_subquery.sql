d704c0d7ea190403e1814cdb5890292f846960dc
Ay-slim <ayooluwaadedipe@gmail.com>
2023-08-16 08:37:22 -0700

    Include ASC flag

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script to list all the cities of Carlifornia
-SELECT id, name FROM cities WHERE id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id;
+SELECT id, name FROM cities WHERE id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
