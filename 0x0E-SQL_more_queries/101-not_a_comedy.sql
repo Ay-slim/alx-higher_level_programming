ccb2b6b4f4760480d9a90d20a3156e550a258cb0
Ay-slim <ayooluwaadedipe@gmail.com>
2023-08-16 13:05:02 -0700

    Correct non comedy list query

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,2 @@
--- List all genres not linked to the Dexter show
-SELECT DISTINCT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_genres.id NOT IN (SELECT id FROM tv_genres WHERE name = 'Comedy') ORDER BY tv_shows.title;
+-- List all non comedy shows
+SELECT DISTINCT tv_shows.title FROM tv_shows WHERE tv_shows.id NOT IN (SELECT tv_shows.id FROM tv_shows JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE tv_genres.name = 'Comedy') ORDER BY tv_shows.title;
