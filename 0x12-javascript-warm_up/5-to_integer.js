f51db35bc988452ab5cdbfd6883e8640c767623e
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-11 07:28:01 -0700

    Remove require for to integer task

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const num = process.argv.length > 2 && Boolean(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : 'Not a number';
-console.log(num);
+console.log(process.argv.length > 2 && Boolean(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : 'Not a number');
