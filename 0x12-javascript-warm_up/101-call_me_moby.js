88ab81e04161a86ad342af30cdc853168d8f8a12
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-11 10:45:59 -0700

    Use function directly

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const callMeMoby = (x, theFunction) => {
+exports.callMeMoby = (x, theFunction) => {
   for (let j = 0; j < parseInt(x); j++) {
     theFunction();
   }
 }
-exports.callMeMoby = callMeMoby;
