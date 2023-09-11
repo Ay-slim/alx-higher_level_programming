3b42b609c9b817cd58aeb4eaf61daa8fffc7a3fb
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-11 07:25:19 -0700

    Remove require

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-const num = argv.length > 2 && Boolean(parseInt(argv[2])) ? parseInt(argv[2]) : 'Not a number';
+const num = process.argv.length > 2 && Boolean(parseInt(process.argv[2])) ? parseInt(process.argv[2]) : 'Not a number';
 console.log(num);
