4706afe853268ab487f7e267bff23b8c32bfd037
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-11 08:23:24 -0700

    Update JavaScript spelling

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const j of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+for (const j of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
   console.log(j);
 }
