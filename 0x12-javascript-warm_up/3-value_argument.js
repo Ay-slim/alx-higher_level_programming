3e295215d521caee39585452a42dd65ee8cba92b
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-11 07:13:25 -0700

    Stop using require

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-const firstArg = argv[2] ?? null;
-if (!firstArg) {
-  console.log('No argument');
-} else {
-  console.log(firstArg);
-}
+console.log(process.argv[2] ?? 'No argument');
