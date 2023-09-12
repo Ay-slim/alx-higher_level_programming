7a58c77b1eb105fa40223d0c894e5d430bcb4c1e
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-12 08:53:26 -0700

    Handle 0 scenario

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -7,6 +7,6 @@ exports.converter = function (base) {
     if (!num) {
       return ans;
     }
-    return recDiv(Math.floor(num / base), { 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' }[String(num % base)] + ans);
+    return recDiv(Math.floor(num / base), { 0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: 'a', 11: 'b', 12: 'c', 13: 'd', 14: 'e', 15: 'f' }[String(num % base)] + ans);
   };
 };
