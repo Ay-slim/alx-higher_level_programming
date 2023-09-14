64b12bc9bc5e4d2775af72b457f0d756727fc61b
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-14 04:08:20 -0700

    Filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module to make a mysql DB call using the MySQLdb library"""
+import MySQLdb
+import sys
+
+
+def db_call(u, pw, db, n):
+    """
+    Use MySQLdb to run a query on the holberton DB for entries matching arg
+    """
+    db = MySQLdb.connect(user=u, passwd=pw, db=db, host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}' ORDER BY id ASC"
+                .format(n))
+    for row in cur.fetchall():
+        print(row)
+
+
+if __name__ == "__main__":
+    db_call(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
