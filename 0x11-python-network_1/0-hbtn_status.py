fefbb311c521fa33f81f8638cc074b8775562f36
Ay-slim <ayooluwaadedipe@gmail.com>
2023-09-29 00:16:24 -0700

    What's my status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module for a script that fetches data with urllib
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(
+            'https://alx-intranet.hbtn.io/status') as response:
+        response_read = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(response.read())))
+        print('\t- content: {}'.format(response_read))
+        print('\t- utf8 content: {}'.format(response.__dict__['msg']))
