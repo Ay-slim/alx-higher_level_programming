a3544dc61fb7ec08dbc3fbc83332b3b652578ea8
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-24 02:18:23 -0700

    Starwars title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const parsedJson = JSON.parse(body);
+    console.log(parsedJson.title);
+  }
+});
