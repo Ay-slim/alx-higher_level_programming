86e8fb36130c89bcb448e1218b8c6a5da39982d7
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-24 01:41:43 -0700

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+request(url, (err, res) => {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(`code: ${res && res.statusCode}`);
+  }
+});
