b039593452124a8a013395f2a4814c342a6d8a5a
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-24 01:45:41 -0700

    semi standard fix for 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,7 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (err, res) => {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
     console.log(`code: ${res && res.statusCode}`);
   }
